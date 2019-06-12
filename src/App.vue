05811553725e72c4fa617795d3009906c018726e
张姣 <zhangjiao@qianbaocard.com>
2019-06-12 20:31:41 +0800

    ratings

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
     <v-header :seller="seller"></v-header>
     <div class="tab border-1px">
       <div class="tab-item"><router-link to="/goods">商品</router-link></div>
-      <div class="tab-item"><router-link to="/remarks">评论</router-link></div>
-      <div class="tab-item"><router-link to="/ratings">商家</router-link></div>
+      <div class="tab-item"><router-link to="/ratings">评论</router-link></div>
+      <div class="tab-item"><router-link to="/remarks">商家</router-link></div>
     </div>
     <router-view :seller="seller"/>
   </div>
